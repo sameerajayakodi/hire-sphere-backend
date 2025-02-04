547415409385e1ec034a4cf2c9395e11be99a449
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-02-04 21:45:39 +0530

    create company controller and routes

---
+++ controllers/companyController.js
@@ -0,0 +1,39 @@
+// Register a new company
+export const registerCompany = async (req, res) => {
+
+}
+
+//company login
+export const loginCompany = async (req, res) => {
+
+}
+
+//Get company Data
+export const getCompanyData = async (req, res) => {
+
+}
+
+//Post a new job
+export const postJob = async (req, res) => {
+
+}
+
+//Get Company Job Applicants
+export const getCompanyJobApplicants = async (req, res) => {
+
+}
+
+//Get Company Posted Jobs
+export const getCompanyPostedJobs = async (req, res) => {
+
+}
+
+//Change Job Application Status
+export const changeJobApplicationStatus = async (req, res) => {
+
+}
+
+//Change Job Visibility
+export const changeVisibility = async (req, res) => {
+
+}
---
+++ routes/companyRoutes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {
+  changeJobApplicationStatus,
+  changeVisibility,
+  getCompanyData,
+  getCompanyJobApplicants,
+  getCompanyPostedJobs,
+  loginCompany,
+  postJob,
+  registerCompany,
+} from "../controllers/companyController";
+
+const router = express.Router();
+
+// Register a new company
+router.post("/register", registerCompany);
+
+//Company login
+router.post("/login", loginCompany);
+
+//Get company Data
+router.get("/company", getCompanyData);
+
+//Post a new job
+router.post("/post-job", postJob);
+
+//Get Company Job Applicants
+router.get("/applicants", getCompanyJobApplicants);
+
+//Get Company Job List
+router.get("/list-jobs", getCompanyPostedJobs);
+
+//Change Job Application Status
+router.post("/change-status", changeJobApplicationStatus);
+
+//Change Application Visibility
+router.post("/change-visibility", changeVisibility);
