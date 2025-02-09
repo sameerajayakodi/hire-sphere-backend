206509b254520d643619ca1cd9d893b9b1c42c4d
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-02-09 22:12:42 +0530

    update route

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import upload from "../config/multer.js";
 import {
   changeJobApplicationStatus,
   changeVisibility,
@@ -9,32 +10,32 @@ import {
   postJob,
   registerCompany,
 } from "../controllers/companyController.js";
-import upload from "../config/multer.js";
+import { protectCompany } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Register a new company
-router.post("/register",upload.single('image'), registerCompany);
+router.post("/register", upload.single("image"), registerCompany);
 
 //Company login
 router.post("/login", loginCompany);
 
 //Get company Data
-router.get("/company", getCompanyData);
+router.get("/company", protectCompany, getCompanyData);
 
 //Post a new job
-router.post("/post-job", postJob);
+router.post("/post-job", protectCompany, postJob);
 
 //Get Company Job Applicants
-router.get("/applicants", getCompanyJobApplicants);
+router.get("/applicants", protectCompany, getCompanyJobApplicants);
 
 //Get Company Job List
-router.get("/list-jobs", getCompanyPostedJobs);
+router.get("/list-jobs", protectCompany, getCompanyPostedJobs);
 
 //Change Job Application Status
-router.post("/change-status", changeJobApplicationStatus);
+router.post("/change-status", protectCompany, changeJobApplicationStatus);
 
 //Change Application Visibility
-router.post("/change-visibility", changeVisibility);
+router.post("/change-visibility", protectCompany, changeVisibility);
 
 export default router;
