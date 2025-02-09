6149cf1884047ba01287d7483f74c6c05a0d4252
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-02-09 22:19:25 +0530

    create job route

---
+++ routes/jobRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import router from "./companyRoutes";
+
+const router = express.Router();
+
+//Route to get all jobs data
+
+//Route to get a single job by ID
+
+export default router;
