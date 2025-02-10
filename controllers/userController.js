c9bf0a3e801a3efe83059935e4576738b0b1e7b4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-02-10 13:28:37 +0530

    create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+// get user data
+
+export const getUserData = async (req, res) => {
+
+}
+
+//apply for a job
+export const applyForJob = async (req, res) => {
+
+}
+
+//get user applied applications
+export const getUserJobApplications = async (req, res) => {
+
+}
+
+//update user profile (resume)
+
+export const updateUserResume = async (req, res) => {
+    
+}
