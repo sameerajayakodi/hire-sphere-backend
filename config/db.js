c1b1814365c07f3e8f4ed4af64fe7ecf8c5aa508
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-02-11 12:57:57 +0530

    fixed mogodb error

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/hire-sphere`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(`${process.env.MONGODB_URI}/hire-sphere`);
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
